4bfae1f2656067743984cc3cc85b13bc3d128966
albychu <albert.chu@yahoo.com>
2021-10-25 00:43:52 -0400

    Updates

--- cypress/integration/examples/Test3.js
+++ cypress/integration/examples/Test3.js
@@ -9,19 +9,21 @@ it('My secondTest case', function () {
     cy.get('#checkBoxOption1').check().should('be.checked').and('have.value', 'option1')
     //"be" is for behavior and "have" for comparison 
     //instead of using multiple ‘should’, you can use ‘and’
-    cy.get('#checkBoxOption1').uncheck().should('not.be.checked’)
-        cy.get('input[type =“checkbox”]’).check([‘option2’, ‘option3’])
+    cy.get('#checkBoxOption1').uncheck().should('not.be.checked')
+    cy.get('input[type ="checkbox"]').check(['option2', 'option3'])
 
-        //Static Dropdown
-        cy.get('select’).select(‘option2’).should(‘have.value’, ‘option2’)
-        //select option and drop down ^   ^ compare and check whether the option is successfully selected or not
+    //Static Dropdown
+    cy.get('select').select('option2').should('have.value', 'option2')
+    //select option and drop down ^   ^ compare and check whether the option is successfully selected or not
 
-        //Dynamic Dropdown
-        cy.get('#autocomplete').type('ind')
-        cy.get('.ui-menu-item div').each(($el, index, $list) => {
+    //Dynamic Dropdown
+    cy.get('#autocomplete').type('ind')
+    cy.get('.ui-menu-item div').each(($el, index, $list) => {
 
-        if ($e1.text() === "India") {
-            $e1.click()
+        if ($el.text() === "India") {
+            $el.click()
         }
-
     })
+
+    cy.get('#autocomplete').should('have.value', 'India')
+})
